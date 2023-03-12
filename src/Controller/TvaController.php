6022dda20e85d9379c6c21fb0b9831616a4dc08d
kharit <hasjok@msn.com>
2023-03-12 19:45:19 +0100

    first commit

--- src/Controller/TvaController.php
+++ src/Controller/TvaController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\TvaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TvaController extends AbstractController
 {
     #[Route('/tva', name: 'app_tva')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('tva/index.html.twig', [
-            'controller_name' => 'TvaController',
+        $form=$this->createForm(TvaType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form -> isValid()) {
+            $data = $form -> getData();
+            $data ['tva']=$data['prix']*0.2;
+            return $this->render('tva/traitement.htmi.twig',[
+                'mes_donnes' => $data
+            ]);
+            
+        }
+
+        return $this->renderForm('tva/index.html.twig', [
+            'le_form_tva' => $form
         ]);
     }
 }
--- src/Form/TvaType.php
+++ src/Form/TvaType.php
@@ -5,13 +5,15 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class TvaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('prix',Type::class)
+        ->add('prix',NumberType::class)
         ->add('calTva',SubmitType::class)
         ;
     }
